ed6cbfdc76662d9e1560829f549d090ab76ebbc7
flaviolzsantos <flaviolzsantos@gmail.com>
2017-03-19 18:46:24 -0300

    Commit inicial

---
+++ WepApiUi/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WepApiUi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "Areas/{area}/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WepApiUi/Areas/Admin/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WepApiUi.Areas.Admin
+{
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        public string Teste()
+        {
+            return "asdasdasdasd";
+        }
+    }
+}
