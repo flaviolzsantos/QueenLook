9554f3ddc6d2dabdf9a8b4df0c6351ea613dc7e9
Rafael Pereira <Rafael Pereira>
2017-04-11 23:48:20 -0300

    Iniciando component Trabalhos Recentes

--- Web.Ui/src/App.js
+++ Web.Ui/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Works from './components/RecentWorks';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="App">
+        <div className="App-menu"></div>
+        <div className="App-recentWorks">
+          <Works />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ Web.Ui/src/components/RecentWorks.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Works extends Component {
+  render() {
+    return (
+        <div className="RecentWork">
+            <div className="left">
+                imagem
+            </div>
+            <div className="right">
+                detalhes
+            </div>
+        </div>
+    );
+  }
+}
+
+export default Works;
